e1af61b1fe28dedc9db762e9af42bb97004fd28a
Matheus Reis <reismatheus97@gmail.com>
2019-02-26 02:59:04 -0300

    Including extra serializable formats and update model mapping

--- catalog/catalog_app/database_setup.py
+++ catalog/catalog_app/database_setup.py
@@ -17,6 +17,7 @@ class Category(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     date_created = Column(DateTime, default=_get_datetime, nullable=False)
+    items = relationship("Item", back_populates='category')
 
     # We added this serialize function to be able to send JSON objects in a serializable format
     @property
@@ -24,8 +25,21 @@ class Category(Base):
         return {
             'id': self.id,
             'name': self.name,
-            'date_created': self.date_created,
+            'date_created': self.date_created
         }
+
+    @property
+    def serialize_with_relations(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'date_created': self.date_created,
+            'items': self.serialize_one2many
+        }
+
+    @property
+    def serialize_one2many(self):
+        return [item.serialize_without_relation for item in self.items]
 
 
 class Item(Base):
@@ -37,7 +51,7 @@ class Item(Base):
     date_created = Column(DateTime, default=_get_datetime, nullable=False)
     owner_mail = Column(String(250), nullable=False)
     category_id = Column(Integer, ForeignKey('category.id'), nullable=False)
-    category = relationship(Category)
+    category = relationship("Category", back_populates="items")
 
     # We added this serialize function to be able to send JSON objects in a serializable format
     @property
@@ -48,6 +62,16 @@ class Item(Base):
             'description': self.description,
             'date_created': self.date_created,
             'category_id': self.category_id,
+            'owner_email': self.owner_mail
+        }
+
+    @property
+    def serialize_without_relation(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'date_created': self.date_created,
             'owner_email': self.owner_mail
         }
 
